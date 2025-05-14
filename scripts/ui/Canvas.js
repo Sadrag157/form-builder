71eb513dd393ab0219edf80b6060c23020307fc5
Sadrag157 <sasha.titan.ch@gmail.com>
2025-05-14 15:14:10 +0200

    updated fields

--- scripts/ui/Canvas.js
+++ scripts/ui/Canvas.js
@@ -1,16 +1,27 @@
 class Canvas {
     constructor() {
         this.dropZone = document.getElementById('dropZone');
+        this.placeholderText=document.getElementById('placeholderText');
         this.fields = [];
         this.setupDropZone();
+        this.updatePlaceholderVisibility();
+    }
+
+    updatePlaceholderVisibility(){
+        if(this.fields.length===0) {
+            this.placeholderText.style.display='block';
+        }else{
+            this.placeholderText.style.display='none';
+        }
     }
 
     addField(fieldType){
-        const field = new Field(fieldType, {label: `Field ${this.fields.length + 1}`})
+        const field = new Field(fieldType, {label: ''})
         this.fields.push(field);
         this.dropZone.appendChild(field.render());
+        this.updatePlaceholderVisibility();
     }
-
+    
     setupDropZone() {
         this.dropZone.addEventListener('dragover', (e) => {
             e.preventDefault();
@@ -18,15 +29,17 @@ class Canvas {
         });
 
         this.dropZone.addEventListener('dragleave', () => {
-            this.dropZone.classList('highight');
+            this.dropZone.classList.remove('highlight');
         })
-
+        console.log(this.dropZone)
         this.dropZone.addEventListener('drop', (e) => {
             e.preventDefault();
             this.dropZone.classList.remove('highlight');
             const fieldType = e.dataTransfer.getData('text/plain');
             if(fieldType){
                 this.addField(fieldType);
+            }else{
+                this.updatePlaceholderVisibility();
             }
         });
     }
